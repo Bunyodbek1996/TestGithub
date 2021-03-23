db8386ed03953ba467fa113d4d8bc9614595b4bb
Jamshidbek_996999623 <jamshidbekmirzaabdullayev@gmail.com>
2021-03-24 00:24:41 +0500

    First test comnit

--- app/src/main/java/com/dasturchi/apilessontwo/MainActivity.java
+++ app/src/main/java/com/dasturchi/apilessontwo/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toast.makeText(this, "salom salom", Toast.LENGTH_SHORT).show();
         photoList = new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerView);
 
